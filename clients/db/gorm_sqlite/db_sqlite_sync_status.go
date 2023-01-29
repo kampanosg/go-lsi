0284b4e579657b705881709ee508688b4b62d0d8
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-29 18:36:46 +0000

    implement sync status methods

--- clients/db/gorm_sqlite/db_sqlite_sync_status.go
+++ clients/db/gorm_sqlite/db_sqlite_sync_status.go
@@ -1,4 +1,26 @@
 package gormsqlite
 
-func (db SqliteSqliteDb) GetLastSyncStatus() (types.SyncStatus, error) {}
-func (db SqliteDb) InsertSyncStatus(ts int64) error                    {}
+import (
+	"time"
+
+	"github.com/kampanosg/go-lsi/models"
+	"github.com/kampanosg/go-lsi/types"
+)
+
+func (db SqliteDb) GetLastSyncStatus() (types.SyncStatus, error) {
+    var result models.SyncStatus
+    db.Connection.Last(&result)
+    if result.ID == 0 {
+        return types.SyncStatus{}, errRecordNotFound
+    }
+    return fromSyncStatusDbRowToType(result), nil
+}
+
+func (db SqliteDb) InsertSyncStatus(ts int64) error {
+    result := db.Connection.Create(models.SyncStatus{ LastRun: time.UnixMilli(ts), Success: true, })
+    return result.Error
+}
+
+func fromSyncStatusDbRowToType(s models.SyncStatus) types.SyncStatus {
+    return types.SyncStatus{ Timestamp: s.LastRun }
+}
