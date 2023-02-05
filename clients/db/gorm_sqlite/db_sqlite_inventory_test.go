04aec3a9e64667e085f59d522d46f73454e2a524
George Kampanos <george.kampanos@nandos.co.uk>
2023-02-05 20:19:11 +0000

    added insert categories unit test

--- clients/db/gorm_sqlite/db_sqlite_inventory_test.go
+++ clients/db/gorm_sqlite/db_sqlite_inventory_test.go
@@ -40,3 +40,46 @@ func TestDbInventory_GetCategories(t *testing.T) {
 		})
 	}
 }
+
+func TestDbInventory_InsertCategory(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		categories  []types.Category
+		hasError    bool
+		expectedLen int
+	}{
+		{"inserts category", []types.Category{{Name: "test", LinnworksID: "test-id-1", SquareID: "square-id-1"}}, false, 1},
+		{"returns error for duplicate keys", []types.Category{
+			{Name: "test 2", LinnworksID: "test-id-2", SquareID: "square-id-2"},
+			{Name: "test 2", LinnworksID: "test-id-2", SquareID: "square-id-2"},
+		}, true, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setup()
+			defer teardown()
+
+			db, err := NewSqliteDb(tmpDb)
+			if err != nil {
+				t.Errorf("failed to open db, err=%s", err.Error())
+			}
+
+			err = db.InsertCategories(tt.categories)
+			if tt.hasError && err == nil {
+				t.Errorf("expecting to throw error")
+			}
+
+			categories, err := db.GetCategories()
+			if err != nil {
+				t.Errorf("unexpected error, got %v", err.Error())
+			}
+
+			if len(categories) != tt.expectedLen {
+				t.Errorf("got %d, want %d", len(categories), tt.expectedLen)
+			}
+		})
+	}
+
+}
