2cf5683d5cdeeefa7d187cd18e1140ab6e9f0d34
George Kampanos <george.kampanos@nandos.co.uk>
2023-02-06 15:44:37 +0000

    tests for get products by sku

--- clients/db/gorm_sqlite/db_sqlite_inventory_test.go
+++ clients/db/gorm_sqlite/db_sqlite_inventory_test.go
@@ -193,3 +193,38 @@ func TestDbInventory_GetProducts(t *testing.T) {
 		})
 	}
 }
+
+func TestDbInventory_GetProductBySku(t *testing.T) {
+	tests := []struct {
+		name            string
+		sku             string
+		hasError        bool
+		expectedProduct types.Product
+	}{
+		{"returns error for invalid sku", "bad-sku", true, types.Product{}},
+		{"returns correct product for ok sku", "sku-001", false, types.Product{Title: "Test Product 1"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setup()
+			defer teardown()
+
+			db, err := NewSqliteDb(tmpDb)
+			if err != nil {
+				t.Errorf("failed to open db, err=%s", err.Error())
+			}
+
+			db.Connection.Save(&models.Product{Title: "Test Product 1", SKU: "sku-001"})
+
+			product, err := db.GetProductBySku(tt.sku)
+			if tt.hasError && err == nil {
+				t.Errorf("expecting to throw error")
+			}
+
+			if tt.expectedProduct.Title != product.Title {
+				t.Errorf("returned wrong product, got %s, want %s", product.Title, tt.expectedProduct.Title)
+			}
+		})
+	}
+}
