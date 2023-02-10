4ad82af5283d5176ef2efc6849ea64a1ee458598
George Kampanos <gkabanos@icloud.com>
2023-02-10 17:27:38 +0000

    more tests

--- clients/db/gorm_sqlite/db_sqlite_order_test.go
+++ clients/db/gorm_sqlite/db_sqlite_order_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/kampanosg/go-lsi/models"
+	"github.com/kampanosg/go-lsi/types"
 )
 
 var (
@@ -45,6 +46,32 @@ func TestDbOrders_WithinRange(t *testing.T) {
 
 			if len(orders) != tt.expectedLen {
 				t.Errorf("got %d, want %d", len(orders), tt.expectedLen)
+			}
+		})
+	}
+}
+
+func TestDbOrders_InsertOrders(t *testing.T) {
+	tests := []struct {
+		name   string
+		orders []types.Order
+		err    error
+	}{
+		{"succeeds when adding order", []types.Order{{SquareID: "test-order-1"}}, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setup()
+			defer teardown()
+
+			db, err := NewSqliteDb(tmpDb)
+			if err != nil {
+				t.Errorf("failed to open db, err=%s", err.Error())
+			}
+
+			if err := db.InsertOrders(tt.orders); err != tt.err {
+				t.Errorf("threw unexpected error, got %s, want %s", err.Error(), tt.err.Error())
 			}
 		})
 	}
